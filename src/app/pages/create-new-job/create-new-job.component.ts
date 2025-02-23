6594c0f307d548c718bfa89fdc4314c2fc99e0d3
Christopher Wells <chrisw@wellswebtech.co.za>
2025-02-23 16:04:54 +0200

    Created most of the Components required
    I created the components using these command lines:
    ng g c login
    ng g c create-new-job
    ng g c home
    ng g c job-details
    ng g c job-listing
    ng g c jobs
    ng g c my-jobs
    ng g c registration

---
+++ src/app/pages/create-new-job/create-new-job.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-job',
+  imports: [],
+  templateUrl: './create-new-job.component.html',
+  styleUrl: './create-new-job.component.css'
+})
+export class CreateNewJobComponent {
+
+}
